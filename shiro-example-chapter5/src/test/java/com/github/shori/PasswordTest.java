62b32cd6fe5b1cc5e37bfa3cf2ffc8e30e5e7ba3
zhuzq <645874679@qq.com>
2017-10-18 11:33:50 +0800

    shori学习五

---
+++ shiro-example-chapter5/src/test/java/com/github/shori/PasswordTest.java
@@ -0,0 +1,83 @@
+package com.github.shori;
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.converters.AbstractConverter;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.junit.Test;
+public class PasswordTest extends BaseTest{
+	@Test
+    public void testPasswordServiceWithMyRealm() {
+        login("classpath:shiro-passwordservice.ini", "wu", "123");
+    }
+
+//	2.1、passwordService使用DefaultPasswordService，如果有必要也可以自定义；
+//	2.2、hashService定义散列密码使用的HashService，默认使用DefaultHashService（默认SHA-256算法）；
+//	2.3、hashFormat用于对散列出的值进行格式化，默认使用Shiro1CryptFormat，另外提供了Base64Format和HexFormat，对于有salt的密码请自定义实现ParsableHashFormat然后把salt格式化到散列值中；
+//	2.4、hashFormatFactory用于根据散列值得到散列的密码和salt；因为如果使用如SHA算法，那么会生成一个salt，此salt需要保存到散列后的值中以便之后与传入的密码比较时使用；默认使用DefaultHashFormatFactory；
+//	2.5、passwordMatcher使用PasswordMatcher，其是一个CredentialsMatcher实现；
+//	2.6、将credentialsMatcher赋值给myRealm，myRealm间接继承了AuthenticatingRealm，其在调用getAuthenticationInfo方法获取到AuthenticationInfo信息后，会使用credentialsMatcher来验证凭据是否匹配，如果不匹配将抛出IncorrectCredentialsException异常。
+//    @Test
+//    public void testPasswordServiceWithJdbcRealm() {
+//        login("classpath:shiro-jdbc-passwordservice.ini", "wu", "123");
+//    }
+
+    @Test
+    public void testGeneratePassword() {
+        String algorithmName = "md5";
+        String username = "liu";
+        String password = "123";
+        String salt1 = username;
+        String salt2 = new SecureRandomNumberGenerator().nextBytes().toHex();
+        int hashIterations = 2;
+
+        SimpleHash hash = new SimpleHash(algorithmName, password, salt1 + salt2, hashIterations);
+        String encodedPassword = hash.toHex();
+        System.out.println(salt2);
+        System.out.println(encodedPassword);
+    }
+
+    @Test
+    public void testHashedCredentialsMatcherWithMyRealm2() {
+        //使用testGeneratePassword生成的散列密码
+       login("classpath:shiro-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+    @Test
+    public void testHashedCredentialsMatcherWithJdbcRealm() {
+
+        BeanUtilsBean.getInstance().getConvertUtils().register(new EnumConverter(), JdbcRealm.SaltStyle.class);
+
+        //使用testGeneratePassword生成的散列密码
+        login("classpath:shiro-jdbc-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+
+    private class EnumConverter extends AbstractConverter {
+        @Override
+        protected String convertToString(final Object value) throws Throwable {
+            return ((Enum) value).name();
+        }
+        @Override
+        protected Object convertToType(final Class type, final Object value) throws Throwable {
+            return Enum.valueOf(type, value.toString());
+        }
+
+        @Override
+        protected Class getDefaultType() {
+            return null;
+        }
+
+    }
+
+    @Test(expected = ExcessiveAttemptsException.class)
+    public void testRetryLimitHashedCredentialsMatcherWithMyRealm() {
+        for(int i = 1; i <= 5; i++) {
+            try {
+                login("classpath:shiro-retryLimitHashedCredentialsMatcher.ini", "liu", "234");
+            } catch (Exception e) {/*ignore*/}
+        }
+        login("classpath:shiro-retryLimitHashedCredentialsMatcher.ini", "liu", "234");
+    }
+}
